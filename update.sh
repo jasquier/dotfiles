111feb57f15599f43f197c45ceb654d0d6ce33e5
John A. Squier <jasquier@gmail.com>
2020-06-17 12:13:36 -0400

    Add .githooks/

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ cp -f /Users/jasquier/.bash_profile .
 cp -f /Users/jasquier/.editorconfig .
 cp -f /Users/jasquier/.gitconfig .
 cp -f /Users/jasquier/.vimrc .
+cp -f /Users/jasquier/.githooks/* .githooks/
 
 echo "Updating contents of brew-list.txt"
 date > brew-list.txt
