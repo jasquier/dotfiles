db8bd16c667b458b80f79828a9011e537c93f1d8
John A. Squier <jasquier@gmail.com>
2020-06-28 10:02:13 -0400

    Clean up scripts using shfmt

--- update.sh
+++ update.sh
@@ -9,5 +9,4 @@ cp -f /Users/jasquier/.githooks/* .githooks/
 cp -f /Users/jasquier/Library/Preferences/com.googlecode.iterm2.plist .
 
 echo "Updating contents of brew-list.txt"
-brew desc $(brew list) > brew-list.txt
-
+brew desc $(brew list) >brew-list.txt
