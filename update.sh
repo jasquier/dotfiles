56c1b1adc1a7ad18cb6b434b4a272f4b1d611350
John A. Squier <jasquier@gmail.com>
2020-10-28 09:46:01 -0400

    Update brew list commands

--- update.sh
+++ update.sh
@@ -9,5 +9,5 @@ cp -f /Users/jasquier/.githooks/* .githooks/
 cp -f /Users/jasquier/Library/Preferences/com.googlecode.iterm2.plist .
 
 echo "Updating contents of brew-list.txt"
-brew desc $(brew list) >brew-list.txt
-brew cask list -1 >>brew-list.txt
+brew desc $(brew list --formula) >brew-list.txt
+brew list --cask -1 >>brew-list.txt
